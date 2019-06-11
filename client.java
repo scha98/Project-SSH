d830c6cd7466987b5bafe66f621eefc1b4f6119b
scha98 <48407425+scha98@users.noreply.github.com>
2019-06-11 21:30:43 +0800

    Create client.java

---
+++ client.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class client {
+    public static void main(String[] args) {
+	System.out.println("Execute shell commands example");
+	System.out.println();
+
+	try {
+	    String cmd = "ssh shahfiq@192.168.182.136";
+	    System.out.println("Executing command: " + cmd);
+	    Process p = Runtime.getRuntime().exec(cmd);
+	    int result = p.waitFor();
+	    
+	    System.out.println("Process exit code: " + result);
+	    System.out.println();
+	    System.out.println("Result:");
+	    BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+	    String line = "";
+	    while ((line = reader.readLine()) != null) {
+		System.out.println(line);
+	    }
+
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+    }
+}
