8406d4165d5981ec341bd9fca88dc08a51c6919d
scha98 <48407425+scha98@users.noreply.github.com>
2019-06-12 10:39:14 +0800

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,19 +2,19 @@ io.write ("Setting Up SSH configuration.")
 print ("Adduser if no user available")
 os.execute("adduser")
 os.execute("service sshd onestart")
-os.execute("echo 'sshd_enable='YES'' >> /etc/rc.conf")
-os.execute("echo 'Port 22' >> /etc/ssh/sshd_config")
+os.execute("echo '#sshd_enable='YES'' >> /etc/rc.conf")
+os.execute("echo '#Port 22' >> /etc/ssh/sshd_config")
 
-os.execute("echo 'Protocol 2' >> /etc/ssh/sshd_config")
+os.execute("echo '#Protocol 2' >> /etc/ssh/sshd_config")
 
-os.execute("echo 'LoginGraceTime 60' >> /etc/ssh/sshd_config")
+os.execute("echo '#LoginGraceTime 60' >> /etc/ssh/sshd_config")
 
-os.execute("echo 'PermitRootLogin no' >> /etc/ssh/sshd_config")
-os.execute("echo 'PasswordsAuthentication yes' >> /etc/ssh/sshd_config")
+os.execute("echo '#PermitRootLogin no' >> /etc/ssh/sshd_config")
+os.execute("echo '#PasswordsAuthentication yes' >> /etc/ssh/sshd_config")
 
-os.execute("echo 'PermitEmptyPassword no' >> /etc/ssh/sshd_config")
+os.execute("echo '#PermitEmptyPassword no' >> /etc/ssh/sshd_config")
 
-os.execute("echo 'UseDNS no' >> /etc/ssh/sshd_config")
+os.execute("echo '#UseDNS no' >> /etc/ssh/sshd_config")
 
 os.execute("ssh-keygen -t rsa")
 print ("Restarting SSH Service")
